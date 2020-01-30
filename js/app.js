4310c29a8e4d96fc19cedb9ddb15522642fa2664
Alexey Babushkin <007-99@mail.ru>
2020-01-31 01:04:15 +0300

    Урок 6 усложненное ДЗ: Замыкание, Рекурсия

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ let isNumber = function (number) {
 
 game();
 
+
 function game(){
 
     function getNumberMath() {
@@ -17,58 +18,93 @@ function game(){
     }
     const numFromGame = getNumberMath();
     const numberGame = numFromGame();
-
-    let enterNumber = prompt('Угадай число от 1 до 100');
-
-    function checkAnsver(){
-        
-        userAnsver(enterNumber);
+ 
+    function counter() {
+        let counter = 3;
+        const decrementCounter = function () {
+            counter = counter -1;
+            return counter; 
+        }
+        return decrementCounter;
     }
 
+    const decrementCounterAnswer = counter();
+
+    let userCounterAnswer;
+
+    function counterAnswer () {
+        const counterAnswer = decrementCounterAnswer();
+        console.log( 'осталось ответов', counterAnswer);
+        userCounterAnswer = counterAnswer;
+        return counterAnswer;
+    }
+
+    userCounterAnswer = counterAnswer();
+    let enterNumber;
+    let finish = false;
+    questNumber()
+
+    function checkAnsver(){
+       userAnsver(enterNumber);
+    }
     checkAnsver();
-
     function userAnsver(enterNumber) {
-
         console.log(enterNumber,numberGame);
-        if (enterNumber === null) {
-            endGame()
-        }
-        (!isNumber(enterNumber)) ? enterText():
+       if (!finish) {
         (+enterNumber === numberGame) ? win() : 
         (+enterNumber < numberGame) ? questNumberMin():
         (+enterNumber > numberGame) ? questNumberMax(): null;
-
+           
+       }            
     }
-
+    function counterZero(n) {
+        if (n == 0) {
+            let answer = confirm('Попытки закончились, хотите сыграть еще?');
+            (answer) ? game() : endGame();
+            
+        }  
+    }
     function win() {
-        alert('Победа');
+        let answer = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?"');
+        console.log(answer);
+        (answer) ? game() : quitGame(); 
     }
-
-    function endGame(params) {
+    function quitGame() {
+        finish = true; 
+        alert (' Спасибо за игру');
+    }
+    function endGame() {
         alert('Вот и поиграли число было : ' + numberGame);
+        quitGame(); 
     }
-
     function questNumber() {
-        enterNumber = prompt('Угадай число от 1 до 100');
-        userAnsver(enterNumber);
+        counterZero(userCounterAnswer);
+        let answer = +prompt('Угадай число от 1 до 100, осталось попыток ...'+ userCounterAnswer);
+        ( !isNumber(answer) ) ? enterText(): (answer === 0) ? endGame():      
+        userAnsver(answer);
     }
     function enterText() {
-        enterNumber = prompt('Введи число!');
-        userAnsver(enterNumber);
+        alert('Введи число!');
+        questNumber();
     }
-
     function questNumberMin() {
-        enterNumber = prompt('Загаданное число больше');
-        userAnsver(enterNumber);
+        counterZero(userCounterAnswer);
+        userCounterAnswer = counterAnswer();
+        let answer = +prompt('Загаданное число больше, осталось попыток ...' + userCounterAnswer);
+        ( !isNumber(answer) ) ? enterText(): (answer === 0) ? endGame():
+        userAnsver(answer);
     }
     function questNumberMax() {
-        enterNumber = prompt('Загаданное число меньше');
-        userAnsver(enterNumber);
+        // counterZero(userCounterAnswer);
+        userCounterAnswer = counterAnswer();
+        counterZero(userCounterAnswer);
+        let answer = +prompt('Загаданное число меньше, осталось попыток ...' + userCounterAnswer);
+        ( !isNumber(answer) ) ? enterText(): (answer === 0) ? endGame():
+        userAnsver(answer);
     }
     
 }
 
-// game();
 
 
 
